4c2c530376bfeeb845ea7cca5e443e2e71ce75ff
Maksym_Rudnyi <maksym_rudnyi@epam.com>
2018-07-23 18:04:35 +0300

    Redux: Refactoring the Entry Point

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import todoApp from './reducers';
-import App from './components/App';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-import {loadState, saveState} from "./loadState";
-import throttle from 'lodash.throttle';
-
-const persistedState = loadState();
-
-const store = createStore(todoApp, persistedState);
-console.log('store2  - ', store.getState());
-
-store.subscribe(throttle(() => {
-	saveState({
-		todos: store.getState().todos,
-	});
-}, 1000));
+const store = configureStore();
 
 render(
-	<Provider store={store}>
-		<App/>
-	</Provider>,
+	<Root store={store} />,
 	document.getElementById('root')
 );
